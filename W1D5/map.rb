25ba1321c5b70337fe7e61c5c48ed54fe9102cac
Jerrik Shaw <jlshaw117@gmail.com>
2018-10-25 22:38:34 -0700

    Add functionality to #initialize #set #get #delete and #show

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,23 +1,39 @@
 class Map
 
   def initialize
-
+    @map = []
   end
 
   def set(key, value)
-
+    if @map.none? {|k,v| k == key}
+      @map << [key, value]
+    else
+      @map.map! do |k,v|
+        if k == key
+          [k, value]
+        else
+          [k,v]
+        end
+      end
+    end
   end
 
   def get(key)
-
+    @map.each {|k,v| return [k,v] if key == k}
   end
 
   def delete(key)
-
+    @map.map! do |k,v|
+      if k == key
+        nil
+      else
+        [k,v]
+      end
+    end.compact
   end
 
   def show
-
+    p @map
   end
 
 end
