b413a57872392f34db60e71d427b2290b8e3283e
Jerrik Shaw <jlshaw117@gmail.com>
2018-10-29 22:50:39 -0700

    Add @name1, @name2 to #initialize Write logic for #make_move and #next_turn

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -1,7 +1,9 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
     @cups = Array.new(14) {Array.new}
     @cups.map!.with_index do |cup, i|
       if i == 6 || i == 13
@@ -25,10 +27,39 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones, cups[start_pos] = cups[start_pos], []
+    current_cup = start_pos
+    until stones.empty?
+      current_cup == 13 ? current_cup = 0 : current_cup += 1
+
+      if current_cup == 13 && current_player_name == @name2
+        drop = true
+      elsif current_cup == 6 && current_player_name == @name1
+        drop = true
+      elsif (0..5).include?(current_cup) || (7 .. 12).include?(current_cup)
+        drop = true
+      else
+        drop = false
+      end
+
+      if drop
+        @cups[current_cup] << stones.pop
+      end
+    end
+    # render
+    next_turn(current_cup)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+
+    if ending_cup_idx ==  13 || ending_cup_idx == 6
+      :prompt
+    elsif cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -40,6 +71,7 @@ class Board
   end
 
   def one_side_empty?
+  
   end
 
   def winner
