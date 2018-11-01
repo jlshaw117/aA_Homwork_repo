48045c57c1f9349a1c659e7c6c35a2bcf66ff63b
Jerrik Shaw <jlshaw117@gmail.com>
2018-10-31 21:14:11 -0700

    Add #dominant_octopus and #clever_octopus and #slow_dance

--- W2D4/O-ctopus.rb
+++ W2D4/O-ctopus.rb
@@ -12,8 +12,46 @@ def sluggish_octopus(arr)
   biggest_fish
 end
 
+def dominant_octopus(arr)
+
+  return arr if arr.length <= 1
+
+  mid = arr.length / 2
+
+  left = dominant_octopus(arr[0...mid])
+  right = dominant_octopus(arr[mid .. -1])
+
+  merge(left,right)
+
+end
+
+def merge(left, right)
+  result = []
+
+  until left.empty? || right.empty?
+    if left[0].length < right[0].length
+      result << left.shift
+    else
+      result << right.shift
+    end
+  end
+  result + left + right
+end
+
+def clever_octopus(arr)
+  arr.reduce("") {|biggest_fish, fish| fish.length > biggest_fish.length ? fish : biggest_fish}
+end
+
+def slow_dance(str, arr)
+  arr.each_with_index do |el, idx|
+    return idx if str == el
+  end
+end
+
 if __FILE__ == $PROGRAM_NAME
   fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
 
   puts sluggish_octopus(fishes)
+  puts dominant_octopus(fishes).last
+  puts clever_octopus(fishes)
 end
