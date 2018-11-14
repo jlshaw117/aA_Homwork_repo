7ddf357ba4e4aac91cb16640cf30c8c7382d6303
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-14 14:00:43 -0800

    Finish homework

--- W4D4/solution/config/environments/development.rb
+++ W4D4/solution/config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.delivery_method = :letter_opener
   # Show full error reports.
   config.consider_all_requests_local = true
 
