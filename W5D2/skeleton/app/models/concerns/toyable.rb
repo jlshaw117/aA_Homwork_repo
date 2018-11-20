6d3461fb31783bafc57a797e3daedf6cf8aa83ed
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-19 22:02:30 -0800

    Finish homework

--- W5D2/skeleton/app/models/concerns/toyable.rb
+++ W5D2/skeleton/app/models/concerns/toyable.rb
@@ -6,11 +6,10 @@ module Toyable
   end
 
   def receive_toy(name)
-    toy = Toy.find_or_create_by(name: name) do |toy|
-      user.last_name = 'Johansson'
+     toy = Toy.find_or_create_by(name: name) do |toy|
+      toy.toyable_type = self.class
+      toy.toyable_id = self.id
     end
-    toy.toyable_type ||= self.class
-    toy.toyable_id ||= self.id
     toy.save
   end
 
