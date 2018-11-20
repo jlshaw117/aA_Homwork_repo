14c59ece6f73823633fe34ebc42066f33162fc19
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-19 21:46:04 -0800

    Add Toyable concern

--- W5D2/skeleton/app/models/concerns/toyable.rb
+++ W5D2/skeleton/app/models/concerns/toyable.rb
@@ -2,10 +2,16 @@ module Toyable
   extend ActiveSupport::Concern
 
   included do
-
+    has_many :toys, as: :toyable
   end
 
   def receive_toy(name)
-
+    toy = Toy.find_or_create_by(name: name) do |toy|
+      user.last_name = 'Johansson'
+    end
+    toy.toyable_type ||= self.class
+    toy.toyable_id ||= self.id
+    toy.save
   end
+
 end
