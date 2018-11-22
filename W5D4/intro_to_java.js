682cb3cc16d380f8b140b20b95187b0cfac8a6c5
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-21 20:39:48 -0800

    Add sumOfNPrimes

--- W5D4/intro_to_java.js
+++ W5D4/intro_to_java.js
@@ -30,3 +30,18 @@ function isPrime(n) {
   }
   return true;
 }
+
+function sumOfNPrimes(n) {
+  let sum = 0;
+  let count = 0;
+  let i = 2;
+
+  while (count < n) {
+    if (isPrime(i)) {
+      sum += i;
+      count++;
+    }
+    i++;
+  }
+  return sum;
+}
