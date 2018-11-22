7549fbd2a49e56dbe378fcf00adecb1e02ff419f
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-21 20:33:59 -0800

    Add fizzbuzz and is prime

--- W5D4/intro_to_java.js
+++ W5D4/intro_to_java.js
@@ -5,3 +5,28 @@ function madLib(verb, adj, noun) {
 function isSubstring(string, substring) {
   return string.includes(substring);
 }
+
+function fizzBuzz (array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
+
+function isPrime(n) {
+  if (n < 2) {
+    return false
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
