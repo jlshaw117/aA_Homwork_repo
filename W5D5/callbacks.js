d4faf0822467ce4a8d35da0060309b360380a4fd
Jerrik Shaw <jlshaw117@gmail.com>
2018-11-22 20:11:03 -0800

    Add #teaAndBiscuts

--- W5D5/callbacks.js
+++ W5D5/callbacks.js
@@ -7,3 +7,55 @@ function hammerTime (time) {
     alert(`${time} is hammer time!`);
   });
 }
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuts () {
+  let firstAns;
+  let secondAns;
+
+  reader.question("Do you want tea?", (res) => {
+    firstAns = res;
+    console.log(`You replied ${res}`);
+  });
+
+  reader.question("Do you want biscuts?", (res) => {
+    firstAns = res;
+    console.log(`You replied ${res}`);
+  });
+
+  let teaRes = (firstAns === `yes`) ? `do` : `don't`;
+  let biscutRes = (secondAns === `yes`) ? `do` : `don't`;
+
+  console.log(`So you ${teaRes} want tea and you ${biscutRes} biscuts.`);
+}
+
+function Cat () {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog () {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat ();
+const Noodles = new Dog ();
+
+// Method style
+
+Noodles.chase(Markov);
+
+Noodles.chase.call(Markov, Noodles);
+Noodles.chase.apply(Markov, [Noodles]);
