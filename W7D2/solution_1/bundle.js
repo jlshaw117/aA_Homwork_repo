b88721c1445fc65b3d3fc7ba740b9612bff566db
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-03 18:19:09 -0800

    Add loggingTo Dispatch function to todo_redux.jsx

--- W7D2/solution_1/bundle.js
+++ W7D2/solution_1/bundle.js
@@ -1206,11 +1206,22 @@ __webpack_require__.r(__webpack_exports__);
 document.addEventListener('DOMContentLoaded', function () {
   var preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
   var store = Object(_store_store__WEBPACK_IMPORTED_MODULE_2__["default"])(preloadedState);
+  store.dispatch = addLoggingToDispatch(store);
   var root = document.getElementById('content');
   react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_root__WEBPACK_IMPORTED_MODULE_3__["default"], {
     store: store
   }), root);
 });
+
+var addLoggingToDispatch = function addLoggingToDispatch(store) {
+  var dispatch = store.dispatch;
+  return function (action) {
+    console.log(store.getState());
+    console.log(action);
+    dispatch(action);
+    console.log(store.getState());
+  };
+};
 
 /***/ }),
 
--- W7D2/solution_1/frontend/todo_redux.jsx
+++ W7D2/solution_1/frontend/todo_redux.jsx
@@ -8,7 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  
+  store.dispatch = addLoggingToDispatch(store);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = (store) => {
+  const dispatch = store.dispatch;
+  return (action) => {
+    console.log(store.getState());
+    console.log(action);
+    dispatch(action);
+    console.log(store.getState());
+  };
+};
