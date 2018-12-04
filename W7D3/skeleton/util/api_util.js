1b47ff69b4eb68ffc0bb682eaa528bf3e82d2903
Jerrik Shaw <jlshaw117@gmail.com>
2018-12-04 13:09:04 -0800

    Add ajax search to api_util.js

---
+++ W7D3/skeleton/util/api_util.js
@@ -0,0 +1,9 @@
+
+function fetchSearchGiphys (searchTerm) {
+    return ($.ajax({
+        method: 'GET',
+        url: `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=2`
+    }));
+}
+
+export default fetchSearchGiphys;
